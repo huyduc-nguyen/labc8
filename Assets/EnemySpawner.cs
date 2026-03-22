d7b7729f88ba9d1347036d57f3a0df84b32eb486
huyduc-nguyen <duytan0123huy@gmail.com>
2026-03-22 23:55:41 +0700

    lab8

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemyVariants; // Kéo các loại Variant vào đây
+    public float spawnTime = 2f; // Cứ 2 giây sinh 1 lần
+
+    void Start()
+    {
+        // Gọi hàm SpawnEnemy liên tục sau mỗi spawnTime
+        InvokeRepeating("SpawnEnemy", 1f, spawnTime);
+    }
+
+    void SpawnEnemy()
+    {
+        // Chọn ngẫu nhiên 1 loại quái trong mảng
+        int randomIndex = Random.Range(0, enemyVariants.Length);
+
+        // Sinh quái tại vị trí của Spawner
+        Instantiate(enemyVariants[randomIndex], transform.position, Quaternion.identity);
+    }
+}
